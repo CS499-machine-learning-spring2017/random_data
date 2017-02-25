53675ddd6e197f0094fe32917842499039df1146
crgant12 <christohper.gant1235@gmail.com>
2017-02-25 18:13:11 -0500

    added columns

--- movingwindow.py
+++ movingwindow.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
 from random import randrange
-from sys import stdout, exit
+from sys import stdout, exit, argv
 
 '''
-This program will generate random data given a specified window
-and will take a specified number for rows
+To run:
+    python movingwindow <windowsize> <rows of data>
+
+Purpose:
+    This program will generate random data given a specified window
+    and will take a specified number for rows
 '''
 
 
 class RandomData:
+    '''Generates random data to replicate what our test data will be
+        returns the row and the average of the row
+        accepts a window size and how many lines of data you want'''
 
     maxnum = 256
 
     def __init__(self, window, linenums):
+        '''
+        window: represents the horizontal ad vertical length of data for the window
+        maxrow: max length of the generated row to save space
+        linesnums: how many lines of data to generate
+        rows = contains rows of data
+        curindex: current index to generate window data from
+        '''
         self.window = window
         self.maxrow = (2 * self.window) - 1
         self.linenums = linenums
@@ -20,16 +34,20 @@ class RandomData:
         self.curindex = 0
 
     def makerows(self):
-        return [ randrange(self.maxnum) for _ in range((self.window * 2) - 1)]
+        '''make rows of random data'''
+        return [ randrange(self.maxnum) for _ in range(self.maxrow)]
 
     def _initrows(self):
+        '''initialize all rows to start generating rows'''
         for _ in range(self.window):
            self.rows.append(self.makerows())
 
     def average(self, row):
+        '''take the average of the given row and returns a float'''
         return ((float(sum(row)) / len(row)))
 
     def _cleanupindex(self):
+        '''function to handle generating new rows of data'''
         self.curindex += 1
         if self.curindex >= self.window:
             self.curindex = 0
@@ -42,26 +60,24 @@ class RandomData:
             and return a tuple of it'''
         for row in self.rows: print row
         newrow = [row[self.curindex: self.curindex + self.window] for row in self.rows ]
-        print newrow
         flattenrow = [item for row in newrow for item in row]
-        print flattenrow
-        print ''
         self._cleanupindex()
         return (flattenrow, self.average(flattenrow))
 
     def getdata(self):
+        '''returns rows of data'''
         self._initrows()
         for _ in range(self.linenums):
             yield self._gendata()
 
 def main():
-    x = RandomData(window = 5, linenums = 10)
-    counter = 1
-    for dataitem in x.getdata():
+    '''only here for testing, but returns data in a csv format'''
+    randdata = RandomData(window = int(argv[1]), linenums = int(argv[2]))
+    for dataitem in randdata.getdata():
         row, average = dataitem
-        stdout.write("\rData entry #{}: {} with averge = {}, len(row) = {}\n".format(counter,row, average, len(row)))
+        row = map(str, row)
+        stdout.write("{},{}\n".format(','.join(row), average))
         stdout.flush()
-        counter += 1
 
 if __name__ == '__main__':
     main()
